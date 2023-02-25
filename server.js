0b915ca970bad8f93b218d6a5fbba67e328cbf12
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-02-25 14:10:45 +0500

    db-done

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 var cors = require("cors");
 const mongooseConnection = require("./helpers/mongoose-connection");
+const appRoutes = require("./routes");
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
