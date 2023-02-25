37a97dad4b34b0b188b24098b006ac4a892a1805
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-02-25 13:45:33 +0500

    update

--- helpers/mongoose-connection.js
+++ helpers/mongoose-connection.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
-const mongoAtlasUri = `mongodb://localhost:27017/basic`;
-
+// const mongoAtlasUri = `mongodb://localhost:27017/basic`;
+const mongoAtlasUri = `mongodb+srv://Danish123:Danish123@cluster0.wlfra1b.mongodb.net/?retryWrites=true&w=majority`;
 function mongooseConnection() {
   try {
     // Connect to the MongoDB cluster
